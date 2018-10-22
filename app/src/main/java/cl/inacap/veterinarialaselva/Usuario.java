6e077667e4dfe8c78ac9b2095a802d14efed220f
Alonso Rojas <contact.alonsorojas@gmail.com>
2018-10-22 20:02:58 -0300

    Proyecto basico con todas las pantallas principales

---
+++ app/src/main/java/cl/inacap/veterinarialaselva/Usuario.java
@@ -0,0 +1,13 @@
+package cl.inacap.veterinarialaselva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
