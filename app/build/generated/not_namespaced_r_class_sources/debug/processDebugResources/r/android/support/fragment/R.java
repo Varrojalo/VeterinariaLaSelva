a98593261a5e991ec2abbc9eb11741565e390bb9
Alonso Rojas <contact.alonsorojas@gmail.com>
2018-10-27 11:35:03 -0300

    Agregados los servicios de google y cambiadas las dependencias de firebase

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
@@ -39,10 +39,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f050060;
-        public static final int notification_icon_bg_color = 0x7f050061;
-        public static final int ripple_material_light = 0x7f05006c;
-        public static final int secondary_text_default_material_light = 0x7f05006e;
+        public static final int notification_action_color_filter = 0x7f050056;
+        public static final int notification_icon_bg_color = 0x7f050057;
+        public static final int ripple_material_light = 0x7f050062;
+        public static final int secondary_text_default_material_light = 0x7f050064;
     }
     public static final class dimen {
         private dimen() {}
@@ -71,18 +71,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f070092;
-        public static final int notification_bg = 0x7f070093;
-        public static final int notification_bg_low = 0x7f070094;
-        public static final int notification_bg_low_normal = 0x7f070095;
-        public static final int notification_bg_low_pressed = 0x7f070096;
-        public static final int notification_bg_normal = 0x7f070097;
-        public static final int notification_bg_normal_pressed = 0x7f070098;
-        public static final int notification_icon_background = 0x7f070099;
-        public static final int notification_template_icon_bg = 0x7f07009a;
-        public static final int notification_template_icon_low_bg = 0x7f07009b;
-        public static final int notification_tile_bg = 0x7f07009c;
-        public static final int notify_panel_notification_icon_bg = 0x7f07009d;
+        public static final int notification_action_background = 0x7f07007d;
+        public static final int notification_bg = 0x7f07007e;
+        public static final int notification_bg_low = 0x7f07007f;
+        public static final int notification_bg_low_normal = 0x7f070080;
+        public static final int notification_bg_low_pressed = 0x7f070081;
+        public static final int notification_bg_normal = 0x7f070082;
+        public static final int notification_bg_normal_pressed = 0x7f070083;
+        public static final int notification_icon_background = 0x7f070084;
+        public static final int notification_template_icon_bg = 0x7f070085;
+        public static final int notification_template_icon_low_bg = 0x7f070086;
+        public static final int notification_tile_bg = 0x7f070087;
+        public static final int notify_panel_notification_icon_bg = 0x7f070088;
     }
     public static final class id {
         private id() {}
@@ -139,7 +139,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0d0048;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0043;
     }
     public static final class style {
         private style() {}
